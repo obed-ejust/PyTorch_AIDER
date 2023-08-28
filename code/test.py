d6c301f47544d76b77f46f44f4001372ed104251
obed-ejust <obed.mogaka@ejust.edu.eg>
2023-08-28 02:33:48 -0700

    Update test.py

--- code/test.py
+++ code/test.py
@@ -12,14 +12,14 @@ saved_model = '../results/model.pth'
 model = torch.load(saved_model)
 model.eval()
 """
-model2 = select_model("ResNet50", 5)
+model2 = select_model("EfficientNet_B0", 5)
 # model = ACFFModel(5)
 # model = select_model("VGG16", 5)
 
 model = ACFFModel(5)
 
-print(model)
-print_no_of_parameter(model)
+# print(model)
+# print_no_of_parameter(model)
 
 # Calculate FLOPs
 # Input example (adjust the size and dimension according to your model)
@@ -27,6 +27,6 @@ input_size = (1, 3, 224, 224)
 # Prepare a dummy input tensor
 dummy_input = torch.randn(*input_size)
 # Use the `profile` function to count FLOPs
-flops, params = profile(model2, inputs=(dummy_input,))
+flops, params = profile(model, inputs=(dummy_input,))
 print(f"FLOPs: {flops / 1e6:.2f} M")
 print(f"Number of parameters: {params / 1e3:.2f} K")
